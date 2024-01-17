7546e763568df0b64fed1e7d6dfef4635b19c79e
manos02 <64806766+manos02@users.noreply.github.com>
2024-01-17 18:52:32 +0100

    fixed offset issue

--- react-web-engineer/src/App.js
+++ react-web-engineer/src/App.js
@@ -25,66 +25,6 @@ function App() {
     });
   }
 
-
-//  if (isLoggedIn){
-//    content = <header>
-//      testeigfigefiugy
-//      <MyButton clicks={clicks} handleClick={handleClick}/>
-//      </header>;
-//  } else {
-//    content= <header>You already clicked the button</header>
-//  }
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload TRYING STUFF.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-//  return (
-//    <div className="App">
-//      <header className='App-tester'><h1>Hello World!</h1>
-//      <form>
-//        <label>
-//          Input:
-//        <input
-//        type="text"
-//        name="email"
-//        placeholder="Email"
-//        />
-//        </label>
-//        <label> Testing
-//        <input
-//        type="text"
-//        name="email"
-//        placeholder="Test"
-//        />
-//        </label>
-//      </form>
-//      <p>Trying stuff</p>
-//      {content}
-//      <p>Trying stuff</p>
-//      <p>Trying stuff</p>
-//      <p>Trying stuff</p>
-//      <p>Trying stuff</p>
-//      <p>Trying stuff</p>
-//      <p>Trying stuff</p>
-//
-//      </header>
-//
-//    </div>
-//  );
 }
 
 export default App;
